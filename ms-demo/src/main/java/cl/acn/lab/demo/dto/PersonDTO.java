925132603e3d07dcf5299bd20e275c81e9e63f02
Gustavo Andrade <gustavo.andrade@accenture.com>
2021-03-11 13:50:59 -0300

    change directory

---
+++ ms-demo/src/main/java/cl/acn/lab/demo/dto/PersonDTO.java
@@ -0,0 +1,17 @@
+package cl.acn.lab.demo.dto;
+
+import lombok.Data;
+
+/**
+ * @author ACN-labs
+ * Date: 09-08-20
+ */
+@Data
+public class PersonDTO {
+
+    private Long id;
+    private String name;
+    private String firstName;
+    private String lastName;
+
+}
